193ac3b1507bce2c1c2ca78e1d9156a91a80d6a0
venus_mae <veemaee@gmail.com>
2020-12-12 17:25:29 -0800

    Update index.js

--- index.js
+++ index.js
@@ -121,28 +121,28 @@ app.post('/users',
   });
 
 //GET ALL USERS
-// app.get('/users', passport.authenticate('jwt', { session: false }), (req, res) => {
-//   Users.find()
-//     .then((users) => {
-//       res.status(201).json(users);
-//     })
-//     .catch((err) => {
-//       console.error(err);
-//       res.status(500).send('Error: ' + err);
-//     });
-// });
+app.get('/users', passport.authenticate('jwt', { session: false }), (req, res) => {
+  Users.find()
+    .then((users) => {
+      res.status(201).json(users);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send('Error: ' + err);
+    });
+});
 
 //GET A USER BY USERNAME
-// app.get('/users/:username', passport.authenticate('jwt', { session: false }), (req, res) => {
-//   Users.findOne({ username: req.params.username })
-//     .then((user) => {
-//       res.json(user);
-//     })
-//     .catch((err) => {
-//       console.error(err);
-//       res.status(500).send('Error: ' + err);
-//     });
-// });
+app.get('/users/:username', passport.authenticate('jwt', { session: false }), (req, res) => {
+  Users.findOne({ username: req.params.username })
+    .then((user) => {
+      res.json(user);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send('Error: ' + err);
+    });
+});
 
 //ALLOW USERS TO UPDATE THEIR USER PROFILE
 app.put('/users/:username',
