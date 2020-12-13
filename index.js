e5e09a6cda38502e220591b011f07906f0fd75fb
venus_mae <veemaee@gmail.com>
2020-12-12 16:37:01 -0800

    Update endpoint to register users

--- index.js
+++ index.js
@@ -80,7 +80,7 @@ app.get('/movies/director/:Name', passport.authenticate('jwt', { session: false 
 
 //ALLOW NEW USERS TO REGISTER
 //Doesn't need authentication as it would allow first-time users to register
-app.post('/users',
+app.post('/register',
 [
   check('username', 'Username is required').isLength({min: 4}),
   check('username', 'Username contains non alphanumeric characters - not allowed.').isAlphanumeric(),
@@ -121,28 +121,28 @@ app.post('/users',
   });
 
 //GET ALL USERS
-app.get('/users', passport.authenticate('jwt', { session: false }), (req, res) => {
-  Users.find()
-    .then((users) => {
-      res.status(201).json(users);
-    })
-    .catch((err) => {
-      console.error(err);
-      res.status(500).send('Error: ' + err);
-    });
-});
+// app.get('/users', passport.authenticate('jwt', { session: false }), (req, res) => {
+//   Users.find()
+//     .then((users) => {
+//       res.status(201).json(users);
+//     })
+//     .catch((err) => {
+//       console.error(err);
+//       res.status(500).send('Error: ' + err);
+//     });
+// });
 
 //GET A USER BY USERNAME
-app.get('/users/:username', passport.authenticate('jwt', { session: false }), (req, res) => {
-  Users.findOne({ username: req.params.username })
-    .then((user) => {
-      res.json(user);
-    })
-    .catch((err) => {
-      console.error(err);
-      res.status(500).send('Error: ' + err);
-    });
-});
+// app.get('/users/:username', passport.authenticate('jwt', { session: false }), (req, res) => {
+//   Users.findOne({ username: req.params.username })
+//     .then((user) => {
+//       res.json(user);
+//     })
+//     .catch((err) => {
+//       console.error(err);
+//       res.status(500).send('Error: ' + err);
+//     });
+// });
 
 //ALLOW USERS TO UPDATE THEIR USER PROFILE
 app.put('/users/:username',
