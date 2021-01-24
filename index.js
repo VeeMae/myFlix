1c6c780322b13132bd9882cc7a203a4595767bc4
venus_mae <veemaee@gmail.com>
2021-01-24 13:36:01 -0800

    Update homepage filepath

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ let auth = require('./auth')(app);
 
 //Homepage
 app.get('/', (req, res) => {
-  res.send('Welcome to myFlix!');
+  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
 });
 
 //RETURN A LIST OF ALL MOVIES
