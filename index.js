792cdf29105ae8e08d322ebdf5ebdeec39381637
venus_mae <veemaee@gmail.com>
2021-01-26 11:38:20 -0800

    Modify index.js file

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ let auth = require('./auth')(app);
 
 //Homepage
 app.get('/', (req, res) => {
-  app.use(express.static('client'));
+  app.use(express.static(path.join(__dirname, '/client', '/dist')));
 });
 
 //RETURN A LIST OF ALL MOVIES
