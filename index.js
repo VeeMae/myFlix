b049aedc042c9b6278c702deb67b1918381f8801
venus_mae <veemaee@gmail.com>
2021-02-09 21:34:56 -0800

    Add a new cors

--- index.js
+++ index.js
@@ -21,7 +21,22 @@ mongoose.connect(process.env.CONNECTION_URI, { useNewUrlParser: true, useUnified
 let auth = require('./auth')(app);
 
 const cors = require('cors');
-app.use(cors());
+// app.use(cors());
+
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+
+    // authorized headers for preflight requests
+    // https://developer.mozilla.org/en-US/docs/Glossary/preflight_request
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    next();
+
+    app.options('*', (req, res) => {
+        // allowed XHR methods
+        res.header('Access-Control-Allow-Methods', 'GET, PATCH, PUT, POST, DELETE, OPTIONS');
+        res.send();
+    });
+});
 
 /* REQUESTS */
 
