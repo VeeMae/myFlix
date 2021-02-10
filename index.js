e419faddbddb45736638af49762053c799964b90
venus_mae <veemaee@gmail.com>
2021-02-09 21:15:30 -0800

    Add CORS

--- index.js
+++ index.js
@@ -21,7 +21,24 @@ mongoose.connect(process.env.CONNECTION_URI, { useNewUrlParser: true, useUnified
 
 let auth = require('./auth')(app);
 
-app.use(cors());
+// app.use(cors());
+
+const allowedOrigins = ['http://localhost:4200'];
+
+app.use(cors({
+  origin: function(origin, callback){
+    // allow requests with no origin
+    // (like mobile apps or curl requests)
+    if(!origin) return callback(null, true);
+    if(allowedOrigins.indexOf(origin) === -1){
+      const msg = 'The CORS policy for this site does not ' +
+                'allow access from the specified Origin.';
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  }
+}));
+
 
 /* REQUESTS */
 
